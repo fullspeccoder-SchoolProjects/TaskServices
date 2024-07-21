c6866fedd697e1ca49018772b7c8be7b420d8812
Jacob Wilson <jcbwlsn04@gmail.com>
2024-07-21 14:29:36 -0500

    Created Task & TaskService classes; Need to implement them into project | v0.0.5

--- src/main/java/com/school/Task.java
+++ src/main/java/com/school/Task.java
@@ -1,4 +1,20 @@
 package com.school;
 
 public class Task {
+    String id;
+    String name;
+    String description;
+    static int classId = 1000;
+
+    Task(String id, String name, String description) {
+        this.id = String.format("%s%n", classId++);
+        this.name = name;
+        this.description = description;
+    }
+
+    Task(Task task) {
+        this.id = String.format("%s%n", classId++);
+        this.name = task.name;
+        this.description = task.description;
+    }
 }
--- src/main/java/com/school/TaskService.java
+++ src/main/java/com/school/TaskService.java
@@ -1,4 +1,26 @@
 package com.school;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class TaskService {
+
+    List<Task> tasks = new ArrayList<>();
+
+    TaskService() {}
+
+    void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    void updateTask(Task task) {
+        tasks.forEach((t) -> {/*Do thing*/});
+    }
+
+    void deleteTask(Task task) {
+//        tasks.removeIf((t) -> {/*Do thing*/});
+    }
 }
